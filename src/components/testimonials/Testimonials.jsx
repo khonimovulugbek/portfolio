c07f94b224b0d8a785ebc9ffb5206ddf660ffe2c
ulugbek9909 <ulugbekkhan94@mail.ru>
2022-11-28 16:47:25 +0500

    finish

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -14,7 +14,7 @@ import {AiOutlineCloudDownload} from "react-icons/ai";
 const pro = [
     {
         id: 1,
-        img: 'https://drive.google.com/uc?id=14Y0HDcIZEm8ygk6b7rePlQfD_xgztATN&export=download',
+        img: 'https://drive.google.com/uc?id=1FEW7HFVJ7Hc0RtZOc7Ivaklj5YP4ZQaP&export=download',
         url: 'https://codewithmosh.com/courses/606251/certificate?pdf=true'
     },
     {
@@ -32,6 +32,17 @@ const pro = [
         img: 'https://drive.google.com/uc?id=1cbjIbqIYziehjHFLcScee2GKOg8v8fNf&export=download',
         url: 'https://s3-us-west-2.amazonaws.com/udacity-printer/production/certificates/4ebdab36-66ec-492a-9831-b71e7124888d.pdf'
     },
+    {
+        id: 5,
+        img: 'https://udemy-certificate.s3.amazonaws.com/image/UC-7c7b8958-ca6b-4667-9577-cae63d58d5aa.jpg',
+        url: 'https://s3-us-west-2.amazonaws.com/udacity-printer/production/certificates/4ebdab36-66ec-492a-9831-b71e7124888d.pdf'
+    },
+    {
+        id: 6,
+        img: 'https://drive.google.com/uc?id=14Y0HDcIZEm8ygk6b7rePlQfD_xgztATN&export=download',
+        url: 'https://codewithmosh.com/courses/606251/certificate?pdf=true'
+    },
+
 ]
 
 const Testimonials = () => {
@@ -39,28 +50,20 @@ const Testimonials = () => {
         <section id='testimonials'>
             <h5>Academic achievement</h5>
             <h2>Certificates</h2>
-            <Swiper className="container testimonials_container"
-                    modules={[Pagination]}
-                    spaceBetween={40}
-                    slidesPerView={1}
-                    pagination={{clickable: true}}
-            >
+            <div className="container portfolio_container">
                 {
-                    pro.map(({id, img,url}) => {
+                    pro.map(({id, img, title, github, demo, short}) => {
                         return (
-                            <SwiperSlide key={id} className="testimonial">
+                            <article key={id} className="portfolio_item">
+                                <div className="portfolio_item-image">
+                                    <img src={img} alt={title}/>
+                                </div>
 
-                                    <div className="testimonial_me-image">
-                                        <img src={img} alt=""/>
-                                    </div>
-                                <a href={url} target='_blank'><AiOutlineCloudDownload/></a>
-
-                            </SwiperSlide>
+                            </article>
                         )
                     })
                 }
-
-            </Swiper>
+            </div>
         </section>
     );
 };
